5cef2b3f4d7cbb0de4fffa78954b446d1570c8f9
Danilo Ávila <daniloavilaf@gmail.com>
2011-06-17 18:34:41 -0300

    geral na apresentacao

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
 pdflatex -output-directory bin presentation.tex
-#bibtex bin/presentation
-#pdflatex -output-directory bin presentation.tex
+bibtex bin/presentation
+pdflatex -output-directory bin presentation.tex
 open bin/presentation.pdf
