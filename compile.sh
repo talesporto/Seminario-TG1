d9d3c93ab53178238e9d1a5177ce27ac5fc40175
Tales Porto <talesap@gmail.com>
2011-06-20 19:51:28 -0300

    Ajustes depois da apresentação

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 pdflatex -output-directory bin presentation.tex
-#bibtex bin/presentation
-#pdflatex -output-directory bin presentation.tex
+bibtex bin/presentation
+pdflatex -output-directory bin presentation.tex
 gnome-open bin/presentation.pdf
